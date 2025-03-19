a4a6cf2ce55a3aeacad1f56d01f2a1dd9d12cdf9
Steven Cheung <stevencheung0690@outlook.com>
2025-03-19 16:14:33 +0000

    chore: init

---
+++ Financial-Tools/Financial-Tools.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+double npv(const double initialInvestment, const double discountRate, const double* cashFlows, const unsigned int n)
+{
+    return 0.0;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ Financial-Tools/Financial-Tools.h
@@ -0,0 +1,17 @@
+#ifndef FINANCIAL_TOOLS_H
+#define FINANCIAL_TOOLS_H
+
+double npv(const double initialInvestment, const double discountRate, const double* cashFlows, const unsigned int n);
+
+// double* cReturns(const double* assetPrices, const unsigned int T);
+
+// double sharpe(const double* assetReturns, const double riskFreeRate,
+	const unsigned int T);
+
+// double* smas(const double* assetPrices, const unsigned int k,
+	const unsigned int T);
+
+// double* prices(const double* meanLogReturns, const double initialPrice,
+	const unsigned int T);
+
+#endif
